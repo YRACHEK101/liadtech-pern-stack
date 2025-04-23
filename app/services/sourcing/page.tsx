0d780c127cc41059d889ee7ab20a8507b750a6dc
Aymane Nahji <aymanenahjidev@gmail.com>
2025-04-23 03:55:56 +0100

    feat: sourcing page without linking to services nabbar options

--- app/services/sourcing/page.tsx
+++ app/services/sourcing/page.tsx
@@ -25,13 +25,13 @@ const features2 = [
 const page = () => {
     return (
         <div>
-            <section className='grid grid-cols-2 px-16 py-10'>
+            <section className='grid grid-cols-2 px-16 py-10 bg-[#2D1F67] text-white '>
                 <div className='flex flex-col gap-7 pt-20 pr-20 z-10 '>
-                    <span className='font-bold text-5xl text-[#2D1F67] '>
+                    <span className='font-bold text-5xl '>
                         Expliquez votre concept
                         en 30 secondes chrono.
                     </span>
-                    <span className=' font-bold text-[#2D1F67] !text-[20px]'>
+                    <span className=' font-bold !text-[20px]'>
                         Attirez l’attention, simplifiez votre message et boostez vos conversions avec une animation 2D claire, moderne et percutante.
                     </span>
                     <div className='flex flex-col '>
@@ -45,7 +45,7 @@ const page = () => {
                         ))}
                     </div>
                     <div className='flex items-center gap-4'>
-                        <div className="text-[85px] font-bold text-[#2D1F67]">750€</div>
+                        <div className="text-[85px] font-bold">750€</div>
                         <Link href="/create-my-app-mobile" className="bg-[#673de6] hover:bg-[#5735bc] flex gap-3.5 items-center text-white rounded-lg px-[31px] py-[10px]">
                             <span>
                                 Profiter de l’offre
@@ -54,9 +54,9 @@ const page = () => {
                         </Link>
                     </div>
                 </div>
-                <Image src={"/images/liadteche site image_sécurité et confornité  1.svg"} width={0} height={0} className='w-auto origin-top-right scale-110 -mt-10  z-0  h-auto' alt='pic' />
+                <Image src={"/images/Sourcing.svg"} width={0} height={0} className='w-auto origin-top-right scale-110 z-0 pl-3 pt-10  h-auto' alt='pic' />
             </section>
-            <section className='bg-[#DCBDFFAA] py-12 px-5 flex flex-col items-center'>
+            <section className=' py-12 px-5 flex flex-col items-center'>
                 <span className='font-bold text-[59px] text-[#2D1F67] mb-2'>
                     Choisissez votre pack Studio Créatif avec Liadtech
                 </span>
@@ -69,18 +69,18 @@ const page = () => {
                     'Cloud, SaaS & Support'
                 ]} />
 
-                <span className='text-[26px] font-light text-[#2D1F67] my-4'>
+                <span className='text-[26px] font-light text-[#673DE6] my-4'>
                     Conditions de paiement
                 </span>
 
             </section>
-            <section className='grid grid-cols-2 px-16 py-10'>
+            <section className='grid grid-cols-2 gap-[78px] px-16 pb-20 bg-[#DCBDFFAA]  '>
 
-                <div className='flex flex-col gap-7  pr-20 '>
+                <div className='flex flex-col gap-7 pt-32 pr-20 '>
                     <span className='font-bold text-5xl text-[#2D1F67] '>
                         Lancez votre marque avec impact, développez sans limites.
                     </span>
-                    <span className=' font-bold text-[#2D1F67] !text-[20px]'>
+                    <span className=' font-bold !text-[20px] text-[#2D1F67] '>
                         Ne perdez plus de temps avec les démarches techniques. Créez, animez et propulsez votre image grâce à nos packs 100% clé-en-main.
                     </span>
                     <div className='flex flex-col gap-4 '>
@@ -90,7 +90,7 @@ const page = () => {
                                     <path d="M12 2L5.63636 13L2 7.4127" stroke="#60BB6F" strokeWidth="4" strokeLinecap="round" strokeLinejoin="round" />
                                 </svg>
                                 <div className='flex flex-col'>
-                                    <span className='!text-[20px] '>{site}</span>
+                                    <span className='!text-[20px]  '>{site}</span>
                                 </div>
                             </div>
                         ))}
@@ -99,12 +99,13 @@ const page = () => {
                         Idéal pour les marques ambitieuses qui veulent se démarquer sur les réseaux et au-delà.
                     </div>
                 </div>
-                <Image src={"/images/Sécurité & Conformité.svg"} width={0} height={0} className='w-auto  -mt-10 h-full' alt='pic' />
+                <Image src={"/images/liadteche site image-11 1.svg"} width={0} height={0} className='w-auto -mt-10    h-full' alt='pic' />
+
             </section>
-            <section className='grid grid-cols-2 px-16 gap-20 py-10'>
-                <Image src={"/images/Maintenance, TMA & MCO section 2.svg"} width={0} height={0} className='w-auto invisible origin-top  !h-full' alt='pic' />
-                <div className='flex flex-col gap-7 pr-20 '>
-                    <span className='font-bold leading-[59px] text-[47px] text-[#2D1F67] '>
+            <section className='grid grid-cols-2 px-16   pb-36  '>
+                        <div></div>
+                <div className='flex flex-col gap-7  pr-20  pt-36  '>
+                    <span className='font-bold text-5xl text-[#2D1F67]'>
                         Donnez vie à votre message avec des animations percutantes
                     </span>
                     <div className='flex flex-col gap-4 '>
