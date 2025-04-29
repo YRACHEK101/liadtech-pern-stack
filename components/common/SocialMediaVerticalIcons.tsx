e69554e2aaab60e28045f2fc9ca9d1965ad581fb
Aymane Nahji <aymanenahjidev@gmail.com>
2025-04-29 14:44:12 +0100

    update SocialMediaVerticalIcons component: add fallback URLs for LinkedIn, Instagram, Facebook, and YouTube to ensure accessibility when environment variables are not set

--- components/common/SocialMediaVerticalIcons.tsx
+++ components/common/SocialMediaVerticalIcons.tsx
@@ -8,16 +8,16 @@ const SocialMediaVerticalIcons = ({className=''}) => {
         "flex flex-col gap-3 items-center justify-center",
         className
     )}>
-        <a href={process.env.NEXT_PUBLIC_LINKEDIN_URL} target="_blank" className='rounded-full' >
+        <a href={process.env.NEXT_PUBLIC_LINKEDIN_URL || "https://www.linkedin.com/company/liadtech"} target="_blank" className='rounded-full' >
             <Image src="/images/Group 201.svg" width={0} height={0} alt='' className='w-auto h-auto' />
         </a>
-        <a href={process.env.NEXT_PUBLIC_INSTAGRAM_URL} target="_blank" className='rounded-full' >
+        <a href={process.env.NEXT_PUBLIC_INSTAGRAM_URL || "https://www.instagram.com/liadtech_?igsh=azljaDNzb3F6YWk3"} target="_blank" className='rounded-full' >
             <Image src="/images/Group 202.svg" width={0} height={0} alt='' className='w-auto h-auto' />
         </a>
-        <a href={process.env.NEXT_PUBLIC_FACEBOOK_URL} target="_blank" className='rounded-full' >
+        <a href={process.env.NEXT_PUBLIC_FACEBOOK_URL || "https://www.facebook.com/share/18z8CL7a2Q/"} target="_blank" className='rounded-full' >
             <Image src="/images/Group 203.svg" width={0} height={0} alt='' className='w-auto h-auto' />
         </a>
-        <a href={process.env.NEXT_PUBLIC_YOUTUBE_URL} target="_blank" className='rounded-full' >
+        <a href={process.env.NEXT_PUBLIC_YOUTUBE_URL || "https://www.youtube.com/@LIADTechOfficial"} target="_blank" className='rounded-full' >
             <Image src="/images/Group 205.svg" width={0} height={0} alt='' className='w-auto h-auto' />
         </a>
     </div>
